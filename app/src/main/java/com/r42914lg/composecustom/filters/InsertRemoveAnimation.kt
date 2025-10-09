95cd4413a3db536ead3c0b54b0e115272dc1c180
Leonid Gomberg <r42914lg@gmail.com>
2025-10-09 11:30:03 +0300

    insert/update animation cleanup

--- app/src/main/java/com/r42914lg/composecustom/filters/InsertRemoveAnimation.kt
+++ app/src/main/java/com/r42914lg/composecustom/filters/InsertRemoveAnimation.kt
@@ -3,7 +3,9 @@ package com.r42914lg.composecustom.filters
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -29,7 +31,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 data class FilterItems(val data: List<Filter> = emptyList())
 
@@ -44,7 +45,6 @@ fun Sample(
     onClick: (Int) -> Unit
 ) {
     val displayItems = remember { mutableStateListOf<DisplayItem>() }
-    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(filters.data) {
         if (displayItems.isEmpty()) {
@@ -53,26 +53,22 @@ fun Sample(
             }
         } else {
             filters.data.forEach { newFilter ->
-                val changedFilter =
-                    displayItems.firstOrNull { it.filter.id == newFilter.id && it.filter.isSelected != newFilter.isSelected }
-
+                val changedFilter = displayItems.firstOrNull {
+                    it.filter.id == newFilter.id && it.filter.isSelected != newFilter.isSelected
+                }
                 changedFilter?.let {
-                    coroutineScope.launch {
-                        changedFilter.animation.animateTo(
-                            targetValue = 0f,
-                            animationSpec = tween(durationMillis = 300, easing = LinearEasing)
-                        )
-                        displayItems.remove(changedFilter)
-                        val anim = Animatable(0f)
-                        val position = if (newFilter.isSelected) 0 else displayItems.count { it.filter.isSelected }
-                        displayItems.add(position, DisplayItem(newFilter, anim))
-                        coroutineScope.launch {
-                            anim.animateTo(
-                                targetValue = 1f,
-                                animationSpec = tween(durationMillis = 300, easing = LinearEasing)
-                            )
-                        }
-                    }
+                    changedFilter.animation.animateTo(
+                        targetValue = 0f,
+                        animationSpec = tween(durationMillis = 300, easing = LinearEasing)
+                    )
+                    displayItems.remove(changedFilter)
+                    val anim = Animatable(0f)
+                    val position = if (newFilter.isSelected) 0 else displayItems.count { it.filter.isSelected }
+                    displayItems.add(position, DisplayItem(newFilter, anim))
+                    anim.animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(durationMillis = 300, easing = LinearEasing)
+                    )
                 }
             }
         }
@@ -87,17 +83,15 @@ fun Sample(
                 displayItems.forEach { item ->
                     key(item.filter) {
                         val scale by item.animation.asState()
-                        if (scale > 0f) {
-                            Box(
-                                modifier = Modifier.graphicsLayer {
-                                    scaleX = scale
-                                    scaleY = scale
-                                    alpha = scale
-                                }
-                            ) {
-                                RenderFilter(item.filter) {
-                                    onClick(it)
-                                }
+                        Box(
+                            modifier = Modifier.graphicsLayer {
+                                scaleX = scale
+                                scaleY = scale
+                                alpha = scale
+                            }
+                        ) {
+                            RenderFilter(item.filter) {
+                                onClick(it)
                             }
                         }
                     }
